908853b0bc934682c3f39de2d3e555494d644d38
Disk1of5 <disk1of5@gmail.com>
2022-10-10 17:53:17 -0600

    Fixing Broken Repo, Init Comit

---
+++ PluginChatMsgCommands.ts
@@ -0,0 +1,15 @@
+import { iPluginChatMsgCommands } from "../Interfaces/PluginChatMsgCommands";
+import {ChatMessageCommands, iChatMsgCommands} from "../Classes/ChatMsgCommands"
+import { Disk } from "./ChatMsgCommands/Disk";
+import { iChatMsgCommandConstructor } from "../Classes/ChatMsgCommandConstructor";
+import { Logger } from "tslog";
+
+export class PluginChatMsgCommands implements iPluginChatMsgCommands{
+    private logger: Logger;
+    public commands: ChatMessageCommands;
+    constructor(config : iChatMsgCommandConstructor, logger: Logger){
+        this.logger=logger;
+        this.commands=new ChatMessageCommands(this.logger);
+        this.commands.addChatMsgCommands(new Disk(config));
+    }
+}
